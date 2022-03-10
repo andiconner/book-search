c7ba917bfd67e8f6f39385468073efe50b0bceee
Andi Conner <andiconner@icloud.com>
2022-03-10 17:57:19 -0500

    getMe query

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
+import { useQuery } from '@apollo/client';
+import { QUERY_GET_ME } from '../utils/queries';
 import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
